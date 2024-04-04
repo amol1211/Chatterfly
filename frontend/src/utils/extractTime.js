8145cd41df8dfa699370f738bcfdf4e4fa678069
Amol Shinde <shindeamol409@gmail.com>
2024-04-04 12:22:19 +0530

    4th part completed - socket

---
+++ frontend/src/utils/extractTime.js
@@ -0,0 +1,25 @@
+export function extractTime(dateString) {
+  const date = new Date(dateString);
+  let hours = date.getHours();
+  const minutes = padZero(date.getMinutes());
+  const period = hours < 12 ? "AM" : "PM";
+  hours = hours % 12 || 12; // Convert to 12-hour format
+  return `${padZero(hours)}:${minutes} ${period}`;
+}
+
+// Helper function to pad single-digit numbers with a leading zero
+function padZero(number) {
+  return number.toString().padStart(2, "0");
+}
+
+/* export function extractTime(dateString) {
+  const date = new Date(dateString);
+  const hours = padZero(date.getHours());
+  const minutes = padZero(date.getMinutes());
+  return `${hours}:${minutes}`;
+}
+
+// Helper function to pad single-digit numbers with a leading zero
+function padZero(number) {
+  return number.toString().padStart(2, "0");
+} */
